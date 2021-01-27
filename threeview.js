57b2ead9e46f70376f7e9c53f53dab29bd08aee8
HDgaming5758 <robinsavouret91@gmail.com>
2021-01-27 06:26:44 +0100

    Three js test and update

--- threeview.js
+++ threeview.js
@@ -12,23 +12,21 @@ function init()
     var height = window.innerHeight;
     renderer.setSize (width, height);
     document.body.appendChild (renderer.domElement);
-
     scene = new THREE.Scene();
-    
-    // start
 
     function createCube(posx, posy, posz) {
-        cube = new THREE.Mesh(new THREE.BoxGeometry(10,10,10));
-        cube.position.set(posx, posy, posz)
+        var cube = new THREE.Mesh(new THREE.BoxGeometry(1,1,1));
+        cube.position.set(posx, posy, posz);
         scene.add(cube);
     }
-    createCube(posx,posy,posz)
 
+    // start
+    createCube(posx,posy,posz);
     // end
 
     camera = new THREE.PerspectiveCamera (45, width/height, 1, 10000);
-    camera.position.y = 160;
-    camera.position.z = 400;
+    camera.position.y = 10;
+    camera.position.z = 10;
     camera.lookAt (new THREE.Vector3(0,0,0));
 
     controls = new THREE.OrbitControls (camera, renderer.domElement);
@@ -36,7 +34,6 @@ function init()
     var gridXZ = new THREE.GridHelper(100, 10);
     gridXZ.setColors( new THREE.Color(0xff0000), new THREE.Color(0xffffff) );
     scene.add(gridXZ);
-
 }
 
 function animate()
