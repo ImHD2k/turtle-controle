17c4b8e48eb8c174c528dbc0254f2dbedc692ab3
HDgaming5758 <robinsavouret91@gmail.com>
2021-01-29 18:14:00 +0100

    Adding console back log

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 5757 });
 
 wss.on('connection', function connection(ws) {
+  console.log('WS : Connected')
   ws.on('message', function incoming(data) {
+    console.log('> ' + data)
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(data);
-        console.log(data)
       }
     });
   });
